b33a8be4dbac2f7b14162ee2d84b33227b9a4b09
yutaf <yutafuji2008@gmail.com>
2016-06-01 19:41:06 +0900

    Add /welcome to routes

--- src/Routes.php
+++ src/Routes.php
@@ -2,5 +2,6 @@
 
 return [
     ['GET', '/', ['App\Controllers\Homepage', 'show']],
+    ['GET', '/welcome', ['App\Controllers\Welcome', 'show']],
     ['GET', '/{slug}', ['App\Controllers\Page', 'show']],
 ];
