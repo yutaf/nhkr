724038bde492932b61e9eec8b77b99e7d279e82f
yutaf <yutafuji2008@gmail.com>
2016-06-15 19:11:32 +0900

    Update login action

--- src/Controllers/Welcome.php
+++ src/Controllers/Welcome.php
@@ -11,7 +11,46 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class Welcome extends Application
 {
+    public function login()
+    {
+        $forms = $this->getForms();
+        $form = $forms['login'];
+        if(isset($form, $_POST[$form->getName()])) {
+            $form->submit($_POST[$form->getName()]);
+
+            if ($form->isValid()) {
+                var_dump('VALID', $form->getData());
+                die;
+            }
+        }
+        $data = [
+            'form_login' => $forms['login']->createView(),
+            'form_signup' => $forms['signup']->createView(),
+            'switch_content_selected_signup' => '',
+            'switch_content_selected_login' => 'switch_content_selected',
+            'class_hide_signup' => 'hide',
+            'class_hide_login' => '',
+        ];
+        $html = $this->renderer->render('Welcome', $data);
+        $this->response->setContent($html);
+    }
+
     public function show()
+    {
+        $forms = $this->getForms();
+        $data = [
+            'form_login' => $forms['login']->createView(),
+            'form_signup' => $forms['signup']->createView(),
+            'switch_content_selected_signup' => 'switch_content_selected',
+            'switch_content_selected_login' => '',
+            'class_hide_signup' => '',
+            'class_hide_login' => 'hide',
+        ];
+        $html = $this->renderer->render('Welcome', $data);
+        $this->response->setContent($html);
+    }
+
+    private function getForms()
     {
         $formBuilderLogin = $this->formFactory->get()->createBuilder()
             ->add('email', TextType::class, array(
@@ -23,29 +62,23 @@ class Welcome extends Application
             ->add('password', PasswordType::class, array(
                 'constraints' => array(
                     new NotBlank(),
-                    new Length(array('min' => 4)),
+                    new Length(array('min' => 6)),
                 ),
             ));
-        $form_login = $formBuilderLogin->getForm()->createView();
+        $form_login = $formBuilderLogin->getForm();
 
         $formBuilderSignup = $formBuilderLogin
             ->add('passwordConfirmation', PasswordType::class, array(
                 'constraints' => array(
                     new NotBlank(),
-                    new Length(array('min' => 4)),
+                    new Length(array('min' => 6)),
                 ),
             ));
-        $form_signup = $formBuilderSignup->getForm()->createView();
+        $form_signup = $formBuilderSignup->getForm();
 
-        $data = [
-            'form_login' => $form_login,
-            'form_signup' => $form_signup,
-            'switch_content_selected_signup' => 'switch_content_selected',
-            'switch_content_selected_login' => '',
-            'class_hide_signup' => '',
-            'class_hide_login' => 'hide',
+        return [
+            'login' => $form_login,
+            'signup' => $form_signup,
         ];
-        $html = $this->renderer->render('Welcome', $data);
-        $this->response->setContent($html);
     }
 }
